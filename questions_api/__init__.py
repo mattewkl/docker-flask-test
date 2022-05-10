4ab7d8eff48c2553fb470ef455689147d30a4585
Mattew <cikle290@gmail.com>
2022-05-10 09:04:09 +0300

    smol refactor

--- questions_api/__init__.py
+++ questions_api/__init__.py
@@ -9,5 +9,7 @@ app.config.from_object(Configuration)
 SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:123456@localhost:5432/postgrestest'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
 from views import bp
 app.register_blueprint(bp)
--- questions_api/views.py
+++ questions_api/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import request, abort, redirect, render_template, url_for
+from flask import request, redirect, render_template, url_for
 from forms import PostForm
 from questions_api import db
 from utilties import get_last_saved_question_in_json, get_questions_by_part
@@ -11,17 +11,14 @@ bp = Blueprint('pages', __name__, template_folder='templates')
 # функция обработчик json словарей, проверка правильности ключа, аборт на 404 при неверном ключе или типе значения.
 @bp.route('/', methods=['POST'])
 def get_questions_json():
-    key, value = list(request.json.items())[0]
-    print(request.json)
-    if key == 'questions_num':
-        try:
-            questions_to_save = int(value)
-        except ValueError:
-            abort(404)
+    content = {}
+    try:
+        questions_to_save = int(request.get('questions_num'))
         get_questions_by_part(questions_to_save, db)
-        return get_last_saved_question_in_json(db)
-    else:
-        abort(404)
+        content = get_last_saved_question_in_json(db)
+    except Exception as e:
+        print(f"Error with {e}")
+    return content
 
 #все то же самое, но с базовым фронтом.
 @bp.route('/questions_form/', methods=['POST', 'GET'])
